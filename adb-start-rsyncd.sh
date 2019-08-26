bf199e1772984858392081399b6ee937e256f354
Tukusej’s Sirs <tukusejssirs@users.noreply.github.com>
2019-08-26 14:53:26 +0200

    Add creation of `rsyncd.conf` to `adb-start-rsyncd.sh`
    - By creating `rsyncd.conf` in `adb-start-rsyncd.sh`, one can simply run `curl -o- https://raw.githubusercontent.com/JBBgameich/rsync-static/master/adb-start-rsyncd.sh | bash` in order to run the script without either cloning the whole repo or downloading two separate files.
    
    - I also added a note to README.md about this.

--- adb-start-rsyncd.sh
+++ adb-start-rsyncd.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-# Download prebuilt rsync
-! [ -f rsync.bin ] && curl --progress-bar -L -o rsync.bin "https://github.com/JBBgameich/rsync-static/releases/download/continuous/rsync-arm"
+# Variables
+rsync_ip="127.0.0.1"
+port="1873"
+android_path_rsync="/"
+
+# Get android arch
+arch=$(adb shell getprop ro.product.cpu.abi)
+case $arch in
+	arm64*|aarch64*)  arch="aarch64"  ;;
+	arm*|aarch*)      arch="arm"      ;;
+	x86*)             arch="x86"      ;;
+esac
+
+# Download pre-built rsync
+! [ -f rsync.bin ] && curl --progress-bar -L -o rsync.bin "https://github.com/JBBgameich/rsync-static/releases/download/continuous/rsync-$arch"
 
 # Kill running rsync instances
 adb shell killall rsync
@@ -9,6 +22,9 @@ adb shell killall rsync
 # Push rsync
 adb push rsync.bin /data/rsync
 adb shell chmod +x /data/rsync
+
+# Create rsyncd.conf
+echo -e "address = $rsync_ip\nport = $port\n[root]\npath = $android_path_rsync\nuse chroot = false\nread only = false\nuid = 0\ngid = 0" > rsyncd.conf
 adb push rsyncd.conf /data/rsyncd.conf
 
 # Start rsync daemon on the device
